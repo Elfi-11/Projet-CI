6eb2fdcf69948a44db0a64ad9a6f7578ab613a1f
Yuyukie <mazars.benjamin@hotmail.fr>
2024-10-22 17:36:30 +0200

    1 er test : Vérification des hp initiaux à 10hp à définir

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -4,44 +4,13 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from personnage import Personnage
 import unittest
+from personnage import Personnage
 
 class TestRpg(unittest.TestCase):
-    def test_10_hp_initiaux(self):
-        personnage = Personnage()
-        self.assertEqual(10, personnage.get_hp())
-
-    def test_attaquer_enleve_1_hp(self): 
-        attaquant = Personnage()
-        defenseur = Personnage()
-        defenseur.recevoir_attaque(attaquant)
-        self.assertEqual(9, defenseur.get_hp())
-    
-    def test_attaquer_enleve_2_hp(self):
-        attaquant = Personnage()
-        defenseur = Personnage()
-        defenseur.recevoir_attaque(attaquant)
-        defenseur.recevoir_attaque(attaquant)
-        self.assertEqual(8, defenseur.get_hp())
-    
-    def test_attaquer_10_fois_tue(self):
-        attaquant = Personnage()
-        defenseur = Personnage()
-
-        for i in range(0,10):
-            defenseur.recevoir_attaque(attaquant)
-        
-        self.assertTrue(defenseur.estMort())
-    
-    def test_attaquer_9_fois_ne_tue_pas(self):
-        attaquant = Personnage()
-        defenseur = Personnage()
-
-        for i in range(0,9):  
-            defenseur.recevoir_attaque(attaquant)
-        
-        self.assertFalse(defenseur.estMort())
+    def test_hp_initiaux(self):
+        test à définir
+       
 
 if __name__ == '__main__':
     unittest.main()
