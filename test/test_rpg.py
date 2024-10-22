fdc003837033be405e6014dc0718f5abecf568c7
Elfii <m.e.dwwm2211@gmail.com>
2024-10-22 16:29:09 +0200

    init

---
+++ test/test_rpg.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from personnage import Personnage
+import unittest
+
+class TestRpg(unittest.TestCase):
+    def test_10_hp_initiaux(self):
+        personnage = Personnage()
+        self.assertEqual(10, personnage.get_hp())
+
+    def test_attaquer_enleve_1_hp(self): 
+        attaquant = Personnage()
+        defenseur = Personnage()
+        defenseur.recevoir_attaque(attaquant)
+        self.assertEqual(9, defenseur.get_hp())
+    
+    def test_attaquer_enleve_2_hp(self):
+        attaquant = Personnage()
+        defenseur = Personnage()
+        defenseur.recevoir_attaque(attaquant)
+        defenseur.recevoir_attaque(attaquant)
+        self.assertEqual(8, defenseur.get_hp())
+    
+    def test_attaquer_10_fois_tue(self):
+        attaquant = Personnage()
+        defenseur = Personnage()
+
+        for i in range(10):
+            defenseur.recevoir_attaque(attaquant)
+        
+        self.assertTrue(defenseur.estMort())
+    
+    def test_attaquer_9_fois_ne_tue_pas(self):
+        attaquant = Personnage()
+        defenseur = Personnage()
+
+        for i in range(9):  # Correction ici pour ne faire que 9 attaques
+            defenseur.recevoir_attaque(attaquant)
+        
+        self.assertFalse(defenseur.estMort())
+
+if __name__ == '__main__':
+    unittest.main()
